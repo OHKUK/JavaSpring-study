96c228342e5c57dbf9be26789c8926c81c8cb7b9
OHKUK <qorhvma97@Naver.com>
2024-12-27 12:58:18 +0900

    2024.12.27
    화면구성, 로그인화면, 로그인 기능추가

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.mysite.sbb.question;
 
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -9,4 +12,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	Question findBySubject(String subject);
 	Question findBySubjectAndContent(String subject, String content);
 	List<Question> findBySubjectLike(String subject);
+	Page<Question> findAll(Pageable pageable);
 }
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,41 +1,39 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.mysite.sbb.answer.Answer;
-import com.mysite.sbb.answer.AnswerRepository;
-import com.mysite.sbb.question.Question;
-import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
 	
-	@Autowired
-	private QuestionRepository questionRepository;
+//	@Autowired
+//	private QuestionRepository questionRepository;
 	
     @Autowired
-    private AnswerRepository answerRepository;
+    private QuestionService questionService;
+//    private AnswerRepository answerRepository;
 
 	@Test
 	void testJpa() {
+
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
 		
 		//답변 데이터를 통해 질문 데이터 찾기 vs 질문 데이터를 통해 답변 데이터 찾기
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//        Optional<Question> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//
+//        List<Answer> answerList = q.getAnswerList();
+//
+//        assertEquals(1, answerList.size());
+//        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 		
 		//답변 데이터 조회하기
 //		  Optional<Answer> oa = this.answerRepository.findById(1);
